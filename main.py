d3db2258b601cd2becdf4a9764f530864caab383
Vivek Ojha <53793159+ViVeKKO@users.noreply.github.com>
2021-05-12 10:12:10 +0530

    main file

---
+++ main.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+Bot = ChatBot(name = 'Calculator',
+                read_only = True,                  
+                logic_adapters = ["chatterbot.logic.MathematicalEvaluation"],                 
+                storage_adapter = "chatterbot.storage.SQLStorageAdapter")
+print('\033c')
+print("Hello, I am a calculator. Give me my work")
+while (True):
+    user_input = input("me: ")
+    if user_input.lower() == 'quit':
+        print("Exiting")
+        break
+    try:
+        response = Bot.get_response(user_input)
+        print("Calculator:", response)
+    except:
+        print("Calculator: Please enter valid input.")
